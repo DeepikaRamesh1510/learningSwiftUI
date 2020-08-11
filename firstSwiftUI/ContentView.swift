7b09f2f8d25d795d91a3cb3ff2e987ec3276e2b5
Deepika <deepika.ram@anywhere.co>
2020-08-11 14:17:44 +0530

    Initial Commit

---
+++ firstSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firstSwiftUI
+//
+//  Created by Deepika on 11/08/20.
+//  Copyright © 2020 Deepika. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
